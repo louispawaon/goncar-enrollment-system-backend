96ef7aff9032e8ee1ed78fcd2b8c23d4d7ddc373
tremor6916 <lpawaon@gmail.com>
2022-08-11 20:32:41 +0800

    update migration sql - updated migration file for test migration akong gibuhat na folder

--- prisma/migrations/20220811123102_goncar_backend_test/migration.sql
+++ prisma/migrations/20220811123102_goncar_backend_test/migration.sql
@@ -1,52 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `registraionnumber` on the `Registrations` table. All the data in the column will be lost.
-  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Trainee` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `UserPreference` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `_CategoryToPost` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[registrationNumber]` on the table `Registrations` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_favoritedById_fkey";
-
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_userPreferenceId_fkey";
-
--- DropForeignKey
-ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_A_fkey";
-
--- DropForeignKey
-ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_B_fkey";
-
--- AlterTable
-ALTER TABLE "Registrations" DROP COLUMN "registraionnumber",
-ADD COLUMN     "registrationNumber" SERIAL NOT NULL;
-
--- DropTable
-DROP TABLE "Category";
-
--- DropTable
-DROP TABLE "Post";
-
--- DropTable
-DROP TABLE "Trainee";
-
--- DropTable
-DROP TABLE "User";
-
--- DropTable
-DROP TABLE "UserPreference";
-
--- DropTable
-DROP TABLE "_CategoryToPost";
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BASIC', 'ADMIN', 'EDITOR');
 
 -- CreateTable
 CREATE TABLE "Trainees" (
@@ -67,6 +20,19 @@ CREATE TABLE "Trainees" (
     "expiryDate" TEXT NOT NULL,
 
     CONSTRAINT "Trainees_pkey" PRIMARY KEY ("traineeId")
+);
+
+-- CreateTable
+CREATE TABLE "Registrations" (
+    "registrationNumber" SERIAL NOT NULL,
+    "traineeId" INTEGER NOT NULL,
+    "courseId" INTEGER NOT NULL,
+    "batchId" INTEGER NOT NULL,
+    "trainingYearSpan" INTEGER NOT NULL,
+    "dateEnrolled" TIMESTAMP(3) NOT NULL,
+    "registrationStatus" TEXT NOT NULL,
+
+    CONSTRAINT "Registrations_pkey" PRIMARY KEY ("traineeId")
 );
 
 -- CreateTable
@@ -169,6 +135,9 @@ CREATE TABLE "roles" (
 CREATE UNIQUE INDEX "Trainees_emailAdd_key" ON "Trainees"("emailAdd");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Registrations_registrationNumber_key" ON "Registrations"("registrationNumber");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Registrations_Batch_batchId_key" ON "Registrations_Batch"("batchId");
 
 -- CreateIndex
@@ -182,6 +151,3 @@ CREATE UNIQUE INDEX "payables_payableId_key" ON "payables"("payableId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "payableNames_payableName_key" ON "payableNames"("payableName");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Registrations_registrationNumber_key" ON "Registrations"("registrationNumber");
