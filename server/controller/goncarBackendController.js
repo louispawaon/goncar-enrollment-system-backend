45580a26a6337a300185fe7ecd3b30986479871f
louispawaon <lpawaon@gmail.com>
2022-08-28 10:41:31 +0800

    update controller - 1.1 to 1.3 done

--- server/controller/goncarBackendController.js
+++ server/controller/goncarBackendController.js
@@ -1,4 +1,86 @@
-import {Request,Response} from 'express';
 import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+/*ENROLLMENT MANAGEMENT*/
+
+//Add Trainee (1.1)
+export const addTrainee = async(req,res) =>{
+    const {firstName,middleName,lastName,birthDay,sex,address,emailAdd,cpNum,educationalAttainment,yearGrad} = req.body;
+    try{
+        const trainee = await prisma.trainees.create({
+            data:{
+                firstName: firstName,
+                middleName: middleName,
+                lastName: lastName,
+                birthDay: birthDay,
+                sex: sex,
+                address: address,
+                emailAdd: emailAdd,
+                cpNum: cpNum,
+                educationalAttainment: educationalAttainment,
+                yearGrad: yearGrad
+            }
+        });
+        res.status(201).json(trainee);
+    }
+    catch(error){
+        res.status(400).json({msg: error.message});
+    }
+};
+
+//Update Trainee (1.2)
+export const updateTrainee = async(req,res)=>{
+    const {firstName,middleName,lastName,birthDay,sex,address,emailAdd,cpNum,educationalAttainment,yearGrad} = req.body;
+    try{
+        const trainee = await prisma.trainees.update({
+            where:{
+                id: Number(req.params.id)
+            },
+            data:{
+                firstName: firstName,
+                middleName: middleName,
+                lastName: lastName,
+                birthDay: birthDay,
+                sex: sex,
+                address: address,
+                emailAdd: emailAdd,
+                cpNum: cpNum,
+                educationalAttainment: educationalAttainment,
+                yearGrad: yearGrad
+            }
+        });
+        res.status(200).json(trainee);
+    }
+    catch(error){
+        res.status(400).json({msg: error.message});
+    }
+};
+
+//Display Trainee (1.3)
+export const displayTrainee = async(req,res)=>{
+    try{
+        const trainee = await prisma.trainees.findUnique({
+            where:{
+                id: Number(req.params.id)
+            }
+        });
+        res.status(200).json(trainee);
+    }
+    catch(error){
+        res.status(400).json({msg: error.message});
+    }
+};
+
+//Create Trainee Registration (1.4)
+export const createTraineeReg = async(req,res)=>{
+    const {SSSNum,TINNum,SGLicense,expiryDate,dateEnrolled,registrationStatus} = req.body;
+    try{
+        const traineeReg = await prisma.trainees.update({
+            
+        });
+    }   
+    catch(error){
+        
+    }
+};
