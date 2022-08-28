6118213fe019865eb03ffe42f16ce2cd34131e2d
louispawaon <lpawaon@gmail.com>
2022-08-28 16:41:35 +0800

    update router - initial import from controller

--- server/routes/goncarBackendRoute.js
+++ server/routes/goncarBackendRoute.js
@@ -2,7 +2,19 @@ import express from "express";
 import {
     addTrainee,
     updateTrainee,
-    displayTrainee
+    displayTrainee,
+    createTraineeReg,
+    updateTraineeReg,
+    displayTraineeReg,
+    deleteTraineeReg,
+    viewTraineeMaster,
+    createCourse,
+    updateCourse,
+    newTrainingYr,
+    updateTrainingYr,
+    viewCourse,
+    viewCourseMaster,
+    viewTrainingYrMaster
 } from "../controller/goncarBackendController.js";
 
 const router = express.Router();
