22f45e4b98d594dd2ff4af733e975d4313ad5af1
louispawaon <lpawaon@gmail.com>
2022-08-28 15:34:07 +0800

    update route - init some stuff

--- server/routes/goncarBackendRoute.js
+++ server/routes/goncarBackendRoute.js
@@ -1,1 +1,10 @@
-import { Express } from "express";
+import express from "express";
+import {
+    addTrainee,
+    updateTrainee,
+    displayTrainee
+} from "../controller/goncarBackendController.js";
+
+const router = express.Router();
+
+export default router;
