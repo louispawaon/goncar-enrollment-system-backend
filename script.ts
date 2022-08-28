38c6bd68035a2d9dbbd77c46bd30062fd146520f
louispawaon <lpawaon@gmail.com>
2022-08-28 15:26:58 +0800

    update script - injection for my part is goodz

--- script.ts
+++ script.ts
@@ -23,33 +23,63 @@ async function main(){
 /*
 const trainee = await prisma.trainees.create({
   data:{
-    firstName:"Try",
-    middleName:"Meh",
-    lastName:"Dela Cruz",
-    birthDay:new Date('1996-01-01'),
-    sex:"Female",
+    firstName:"Louis",
+    middleName:"Jaboc",
+    lastName:"Pawaon",
+    birthDay:new Date('2001-10-16'),
+    sex:"Male",
     address:"Davao City",
-    emailAdd:"juandcruz@testtest.com",
+    emailAdd:"lpawaon@gmail.com",
     cpNum:"0912343533222",
     educationalAttainment:"College Graduate",
     yearGrad:'2022-05-01',
-    SSSNum:"12312-3123213-3213",
-    TINNum:"1312321-123213-12321",
-    SGLicense:"123123-312312-31231",
-    expiryDate:new Date('2022-05-01')
 }});
 
 console.log(trainee)
+*/
+/*
+const batch = await prisma.batch.create({
+  data:{
+    laNumber:"123-123-132",
+    batchName:"Agila",
+    startDate: new Date('2022-10-18'),
+    endDate: new Date ('2022-10-22'),
+    maxStudents: Number(30)
+  }
+});
+
+const course = await prisma.courses.create({
+  data:{
+      courseName:"PLTC",
+      courseDescription:"Something something details",
+      requiredHours: Number(120),
+      units: Number(5)
+  }
+});
 
 const traineeRegistration = await prisma.trainees.update({
   where:{
     traineeId:1
   },
   data:{
+    SSSNum:"12312-3123213-3213",
+    TINNum:"1312321-123213-12321",
+    SGLicense:"123123-312312-31231",
+    expiryDate:new Date('2022-05-01'),
     registrations:{
       create:{
         dateEnrolled:new Date('2022-02-01'),
         registrationStatus:"enrolled",
+        batch:{
+          connect:{
+            batchId:1
+          }
+        },
+        courses:{
+          connect:{
+            courseId:1
+          }
+        }
       }
     }
   }
@@ -68,6 +98,32 @@ const traineeShow = await prisma.trainees.findUnique({
 
 console.log(traineeShow)
 */
+
+/*const findtime = await prisma.courses.findMany({
+  include:{
+    registration: true,
+    batch:true
+  }
+})
+console.log(JSON.stringify(findtime))*/
+/*const trainingYear = await prisma.trainingYears.create({
+  data:{
+    trainingYearSpan:"2022-2023"
+  }
+});
+
+const registration = await prisma.registrations.update({
+  where:{
+    registrationNumber:1
+  },
+  data:{
+    trainingYears:{
+      connect:{
+        trainingYearId:1
+      }
+    }
+  }
+})*/
 }
 
 main()
