e85e438af1d5b9871304c6b45af0a57434cafd5a
louispawaon <lpawaon@gmail.com>
2022-08-29 14:41:36 +0800

    update index - storage for now

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import goncarBackendRoute from './server/routes/goncarBackendRoute.js';
 
+
+
 dotenv.config();
 
+const port = process.env.PORT||5000;
 const app = express();
 
 app.use(cors);
 app.use(express.json());
 app.use(goncarBackendRoute);
 
-app.listen(process.env.BACKEND_PORT||3000,  () =>
-  console.log('Server Running'),
+app.listen(port,  () =>
+  console.log(`Server Running at: ${port}`),
 )
