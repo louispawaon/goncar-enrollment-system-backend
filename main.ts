74ec66d089beac3b5c6d0016c19aa74e6cbbeb16
louispawaon <lpawaon@gmail.com>
2022-08-30 13:32:01 +0800

    update main - kapila na nako ni na push tf

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({origin:'http://127.0.0.1:3000'}))
+app.use(cors({origin:'http://localhost:3000'}))
 app.use(express.json());
 
 /*ENROLLMENT MANAGEMENT*/
