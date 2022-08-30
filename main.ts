042fa9a7e6a9b1fb71543f449c42cd05d4f15e76
louispawaon <lpawaon@gmail.com>
2022-08-30 15:50:29 +0800

    update main

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({origin:'http://localhost:3000'}))
+app.use(cors({origin:'http://localhost:3000'||'http://localhost:6006'}))
 app.use(express.json());
 
 /*ENROLLMENT MANAGEMENT*/
