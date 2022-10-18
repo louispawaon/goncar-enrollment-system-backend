7d2d13679625c5487d1e4d9b5c56348fb7dded13
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-10-18 17:16:40 +0800

    else

--- main.ts
+++ main.ts
@@ -543,42 +543,42 @@ app.put('/api/trainees/:id/registrations/:regid/',async(req:Request,res:Response
                 throw "hasRemainingBalance"
             }
 
-        } //dre mag end
-        else{
-            const trainee = prisma.trainees.update({
-                where:{
-                    traineeId:Number(req.params.id)
+        }
+
+        //dre mag end
+        const trainee = prisma.trainees.update({
+            where:{
+                traineeId:Number(req.params.id)
+            },
+            data:{
+                SSSNum: SSSNum,
+                TINNum: TINNum,
+                SGLicense: SGLicense,
+                expiryDate: expiryDate ? new Date(expiryDate) : null
+            }
+        });
+
+        const traineeReg = prisma.registrations.update({
+            where:{
+                registrationNumber:Number(req.params.regid)
+            },
+            data:{
+                dateEnrolled: dateEnrolled,
+                registrationStatus: registrationStatus,
+                trainees:{
+                    connect:{
+                        traineeId:Number(req.params.id)
+                    }
                 },
-                data:{
-                    SSSNum: SSSNum,
-                    TINNum: TINNum,
-                    SGLicense: SGLicense,
-                    expiryDate: expiryDate ? new Date(expiryDate) : null
-                }
-            });
-    
-            const traineeReg = prisma.registrations.update({
-                where:{
-                    registrationNumber:Number(req.params.regid)
-                },
-                data:{
-                    dateEnrolled: dateEnrolled,
-                    registrationStatus: registrationStatus,
-                    trainees:{
-                        connect:{
-                            traineeId:Number(req.params.id)
-                        }
-                    },
-                    batch:{
-                        connect:{
-                            batchId:batchId
-                        }
+                batch:{
+                    connect:{
+                        batchId:batchId
                     }
                 }
-            });
-    
-            await prisma.$transaction([trainee,traineeReg]); 
-        }
+            }
+        });
+
+        await prisma.$transaction([trainee,traineeReg]); 
     
         // set hasActiveRegistration to FALSE in trainee IF NO ACTIVE REG
         const activeReg = await prisma.trainees.findMany({
